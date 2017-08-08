e8e2b58010475ab8774fe84ba518a64c72dc5556
rdhlb <maxradohleb@gmail.com>
2017-08-08 16:33:51 +0300

    add new app architecture

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { askName } from '..';
+import { askName } from '../utils';
 
 console.log('Welcome to the Brain Games!\n');
 
---
+++ src/games/brain-even.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+import { askName, generateNumber, isEven } from '../utils';
+
+const askNumber = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+
+  const userName = askName();
+
+  const iter = (counter) => {
+    // set the number of questions at line below
+    if (counter === 3) {
+      return console.log(`Congratulations, ${userName}!`);
+    }
+
+    const randomNum = generateNumber(1, 100); // set the range of numbers (min, max)
+    console.log(`Question: ${randomNum}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((isEven(randomNum) && answer === 'yes') || (!isEven(randomNum) && answer === 'no')) {
+      console.log('Correct!');
+      return iter(counter + 1);
+    }
+
+    return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(randomNum) ? 'yes' : 'no'}'.\n Let's try again, ${userName}!`);
+  };
+
+  return iter(0);
+};
+
+export default askNumber;
