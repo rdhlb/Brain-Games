53680c282602bfaca5d52818dfe5adba80e66dd5
rdhlb <maxradohleb@gmail.com>
2017-08-08 15:41:22 +0300

    refactor isEven function

--- src/index.js
+++ src/index.js
@@ -8,10 +8,4 @@ export const askName = () => {
 
 export const generateNumber = (min, max) => Math.floor(min + (Math.random() * ((max + 1) - min)));
 
-export const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
-
-  return false;
-};
+export const isEven = num => num % 2 === 0;
