7d865a248f8c83690dbf12ae7b38a94295729119
rdhlb <maxradohleb@gmail.com>
2017-08-10 19:36:51 +0300

    refactor game engine

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,4 +1,4 @@
-import { cons, car, cdr } from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 import { makeGame } from '..';
 import generateNumber from '../utils';
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import { car, cdr } from 'hexlet-pairs';
 
 export const iterationsCount = 3; // set the number of questions
 
+const iter = (counter, userName, generateGameData) => {
+  if (counter === iterationsCount) {
+    return console.log(`Congratulations, ${userName}!`);
+  }
+
+  const gameData = generateGameData();
+  const question = car(gameData);
+  const calculatedAnswer = cdr(gameData);
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  if (userAnswer === calculatedAnswer) {
+    console.log('Correct!');
+    return iter(counter + 1, userName, generateGameData);
+  }
+
+  return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${calculatedAnswer}'.\nLet's try again, ${userName}!\n`);
+};
+
 export const makeGame = (generateGameData, rule) => {
   console.log('Welcome to the Brain Games!');
 
@@ -11,24 +30,5 @@ export const makeGame = (generateGameData, rule) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
-  const iter = (counter) => {
-    if (counter === iterationsCount) {
-      return console.log(`Congratulations, ${userName}!`);
-    }
-
-    const gameData = generateGameData();
-    const question = car(gameData);
-    const calculatedAnswer = cdr(gameData);
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === calculatedAnswer) {
-      console.log('Correct!');
-      return iter(counter + 1);
-    }
-
-    return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${calculatedAnswer}'.\nLet's try again, ${userName}!\n`);
-  };
-
-  return iter(0);
+  return iter(0, userName, generateGameData);
 };
